f5d962f65e1ac8eb4e917b16f36de264979308b8
ca-kraa <alexandrokingnabil@gmail.com>
2023-08-23 10:26:29 +0700

    Jobs Queue - Nabil Mufti

---
+++ app/Http/Controllers/GetDataInvestProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\DispatchDataInvestProduct;
+use Illuminate\Http\Request;
+
+class GetDataInvestProductController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        DispatchDataInvestProduct::dispatch();
+
+        return response()->json(['message' => 'job running in background']);
+    }
+}
